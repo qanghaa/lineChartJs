2090fbbbc5af1991a09b0cf6ffb035eb1ed130b7
Quang Ha <quangha.antibug@gmail.com>
2021-10-23 21:33:02 +0700

    update code vjp pro more

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ const updateData = (chart, max, min, stepValue, typeChart) => {
   });
 }
 
+const createChart = function (idElement, config ) {
+  new Chart(
+  document.getElementById(idElement),
+  config
+)}
+
 const randomValuesFollowArrange = (preValue, nextValue, stepValue) => (preValue > nextValue) ? (preValue - Math.random() * (stepValue - 0)) : (preValue + Math.random() * (stepValue - 0));
 
 const checkOutOfAverage = (typeProblem) => {
@@ -33,6 +39,7 @@ const temporaryData = {
     data: [],
   }]
 };
+
 const heartRateData = {
   datasets: [{
     label: `Biểu đồ nhịp tim ngày ${date}`,
@@ -41,6 +48,7 @@ const heartRateData = {
     data: []
   }]
 };
+
 const sp02Data = {
   datasets: [{
     label: `Biểu đồ số Oxy trong máu ngày ${date}`,
@@ -88,24 +96,138 @@ const temporaryConfig = {
 };
 
 
-var myChart = new Chart(
-document.getElementById('myChart1'),
-temporaryConfig
-);
+const realtimeTemperatureChart = createChart('realtime-chart-one', temporaryConfig)
 
 const heartRateConfig = JSON.parse(JSON.stringify(temporaryConfig));
 heartRateConfig.data = heartRateData
 heartRateConfig.options.scales.x.realtime.onRefresh = myChart2 => updateData(myChart2, 100, 60, 3, "Heart Rate");
-var myChart2 = new Chart(
-document.getElementById('myChart2'),
-heartRateConfig
-);
+const realtimeHeartRateChart = createChart('realtime-chart-two', heartRateConfig)
 
 const sp02Config = JSON.parse(JSON.stringify(temporaryConfig));
 sp02Config.data = sp02Data
 sp02Config.options.scales.x.realtime.onRefresh = myChart3=> updateData(myChart3, 1, 0.95, 0.003, "Blood Oxygen Levels")
-var myChart3 = new Chart(
-document.getElementById('myChart3'),
-sp02Config
-);
+const realtimeSp02Chart = createChart('realtime-chart-three', sp02Config)
 
+const labels = ['7 Ngày trước', '6 Ngày trước', '5 Ngày trước','4 Ngày trước','3 Ngày trước','Hôm kia','Hôm qua'];
+
+const normalTemperatureConfig = {
+  labels: labels,
+  datasets: [{
+    label: 'Nhiệt độ trung bình theo ngày',
+    data: [36.9, 37.1, 36.7, 36.9, 38, 39, 37.5],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(20, 48, 76, 0.2)'
+    ],
+    borderColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(153, 102, 255)',
+      'rgb(117, 105, 193)'
+    ],
+    borderWidth: 1
+  }]
+};
+
+const normalSp02Config = {
+  labels: labels,
+  datasets: [{
+    label: 'Tỉ số Oxy trong máu trung bình',
+    data: [90, 91, 91.5, 92, 94, 88, 90],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(20, 48, 76, 0.2)'
+    ],
+    borderColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(153, 102, 255)',
+      'rgb(117, 105, 193)'
+    ],
+    borderWidth: 1
+  }]
+};
+
+const normalHeartRateConfig = {
+  labels: labels,
+  datasets: [{
+    label: 'Nhịp tim theo phút trung bình',
+    data: [86, 78, 79, 90, 85, 80, 77],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(20, 48, 76, 0.2)'
+    ],
+    borderColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(153, 102, 255)',
+      'rgb(117, 105, 193)'
+    ],
+    borderWidth: 1
+  }]
+};
+// const normalHeartRateConfig = {
+
+// }
+// const normalSp02Config = {
+
+// }
+
+const createNormalObjectConfig = (objectRange, data) =>  normalConfig = {
+  type: 'bar',
+  data: data,
+  options: {
+    scales: {
+      y: objectRange
+    }
+  }
+};
+
+const normalTemperatureConfigObject = createNormalObjectConfig({
+  min:30,
+  max:45,
+  title: {
+    display: true,
+    text: 'Nhiệt độ'
+  }
+}, normalTemperatureConfig)
+const normalTemperatureChart = createChart('normal-chart-one', normalTemperatureConfigObject);
+
+const normalHeartRateConfigObject = createNormalObjectConfig({min:50, max:120,title: {
+  display: true,
+  text: 'Nhịp tim trên phút'
+}}, normalHeartRateConfig)
+const normalHeartRateChart = createChart('normal-chart-two', normalHeartRateConfigObject);
+
+const normalSp02ConfigObject = createNormalObjectConfig({
+  min:0, 
+  max:100, 
+  title: {
+  display: true,
+  text: 'Tỉ số Oxy trong máu'
+}}, normalSp02Config)
+const normalSp02Chart = createChart('normal-chart-three', normalSp02ConfigObject);
